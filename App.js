17e75e9d03a4fdcfa27628e0df9f253094937fbd
hayat hani-tikass <hayathani-tikass@hayats-MacBook-Air.local>
2024-05-31 12:07:25 +0000

    update

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+} from "react-native";
+import React from "react";
+import { categoriesData } from "./mockData/categories.data";
+import { taskData } from "./mockData/task.data";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={{flex: 1, backgroundColor: "#F7F0E8"}}>
+      <View  style={{ gap: 20, padding: 15 }}>
+        <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
+          <View>
+            <Text style={{fontSize: 30, fontWeight: "bold"}}>Hello, Devs</Text>
+            <Text>14 tasks devs</Text>
+          </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <TouchableOpacity style={{backgroundColor: "white" , borderRadius: 40, padding: 5}}>
+            <Image source={require("./assets/person.png")} />
+          </TouchableOpacity>
+        </View>
+
+        {/**Search Bar */}
+        <View style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+          {/**TextInput Box  */}
+          <TextInput
+            placeholder="Search"
+            style={{
+              backgroundColor: "white",
+              width: "80%",
+              height: 50,
+              borderRadius: 10,
+              padding: 5,
+            }}
+          />
+          {/**FIlter Icon */}
+          <View style={{backgroundColor: "#F0522F", padding: 10, borderRadius: 10}}>
+            <Image source={require("./assets/Vector.png")} />
+          </View>
+        </View>
+
+        {/**Categories */}
+        <View style={{ gap: 10 }}>
+          <Text style={{fontSize: 30, fontWeight: "bold"}}>Categories</Text>
+
+          <FlatList
+            data={categoriesData}
+            renderItem={({ item }) => (
+              <View
+                style={{ gap: 10, backgroundColor: "white", marginRight: 5, marginLeft: 5, borderRadius: 20, padding: 25 }}
+              >
+                <View>
+                  <Text style={{fontWeight: "bold", fontSize: 25}}>
+                    {item.name}
+                  </Text>
+                  <Text>{item.description}</Text>
+                </View>
+                <Image source={item.image} />
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+
+        {/**Ongoing Tasks */}
+        <View style={{ gap: 20 }}>
+          <Text style={{fontWeight: "bold", fontSize: 25}}>Ongoing Task</Text>
+
+          <FlatList
+            data={taskData}
+            renderItem={({ item }) => (
+              <View
+                style={{ gap: 10, height: 150, backgroundColor: "white", borderColor: "#E8D1BA", marginRight: 10, padding: 20, borderRadius: 20, width:"100%", marginBottom: 5, justifyContent: "center"  }}
+              >
+                <View>
+                  <Text style={{fontWeight: "600", fontSize: 20}} >
+                    {item.name}
+                  </Text>
+                </View>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ mockData/categories.data.jsx
@@ -0,0 +1,26 @@
+export const categoriesData = [
+  {
+    id: 0,
+    name: "Exercise",
+    description: "12 Task",
+    image: require("../assets/woman1.png"),
+  },
+  {
+    id: 1,
+    name: "Study",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 2,
+    name: "Program",
+    description: "12 Task",
+    image: require("../assets/woman1.png"),
+  },
+  {
+    id: 3,
+    name: "learn",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+];
